52425dd0b7b9ab2b1fd1459b1779e83c38fa694c
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-11-20 19:34:38 +0100

    arraycopy

--- src/main/java/Main8.java
+++ src/main/java/Main8.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Main8 {
+    public static void main(String[] args) {
+        int[][] table = {
+                {0, 1, 2, 3, 4},
+                {0, 1, 2, 3, 4},
+                {0, 1, 2, 3, 4},
+                {0, 1, 2, 3, 4},
+        };
+
+
+        System.out.println(Arrays.toString(lessMore(table)));
+    }
+
+    static int[] lessMore(int[][] arr) {
+        int average = 0;
+        int sum = 0;
+        int arrTableSize = 0;
+        int numberOfElementsBiggerThanAverage = 0;
+        int numberOfElementsSmallerThanAverage = 0;
+        int[] result = new int[2];
+
+        for (int[] ints : arr) {
+            for (int j = 0; j < arr.length; j++) {
+                sum += ints[j];
+                arrTableSize++;
+            }
+        }
+        average = sum / arrTableSize;
+        for (int[] ints : arr) {
+            for (int j = 0; j < arr.length; j++) {
+                if (ints[j] > average) {
+                    numberOfElementsBiggerThanAverage++;
+                } else if (ints[j] < average) {
+                    numberOfElementsSmallerThanAverage++;
+                }
+            }
+
+
+        }
+        System.out.println(average);
+        result[0] = numberOfElementsSmallerThanAverage;
+        result[1] = numberOfElementsBiggerThanAverage;
+        return result;
+    }
 }
---
+++ src/main/java/Main9.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Main9 {
+    public static void main(String[] args) {
+        int[] table = {1, 2, 4, 5};
+
+        System.out.println(Arrays.toString(append(table)));
+    }
+
+    static int[] append(int[] arr) {
+        int[] reversed = new int[arr.length];
+        int[] tab = Arrays.copyOf(arr, arr.length * 2);
+        int k = tab.length - arr.length;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            reversed[i] = arr[arr.length - 1 - i];
+        }
+        System.out.println(Arrays.toString(reversed));
+
+        int[] arrTmp = new int[tab.length - k + reversed.length];
+        System.arraycopy(tab, 0, arrTmp, 0, tab.length - k);
+        System.arraycopy(reversed, 0, arrTmp, tab.length - k, reversed.length);
+        return arrTmp;
+    }
+}
+
+//    public static void arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
+//
+//        src − This is the source array.
+//
+//                srcPos − This is the starting position in the source array.
+//
+//                dest − This is the destination array.
+//
+//                destPos − This is the starting position in the destination data.
+//
+//                length − This is the number of array elements to be copied.
+//
