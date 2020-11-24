526a1a4a94bf7d0c26c8a2be76f5fd03990c1cad
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-11-24 06:49:56 +0100

    using scanner to read file content

---
+++ src/main/java/day_4/Main7.java
@@ -0,0 +1,20 @@
+package day_4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+
+        File file = new File("/home/przemysaw/IdeaProjects/tests_junit/Homework_Java_01/src/main/java/day_4/main4.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine())  ;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
