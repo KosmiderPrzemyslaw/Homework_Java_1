88dc06076d0ba91d77845f7b1e7cc82b184e07fe
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-11-25 22:39:49 +0100

    use fileWriter

--- src/main/java/day_4/Main11.java
+++ src/main/java/day_4/Main11.java
@@ -1,6 +1,8 @@
 package day_4;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Main11 {
@@ -13,6 +15,19 @@ public class Main11 {
         }
         printWriter.println("first line");
         printWriter.println("second line");
+        printWriter.write("use print write method");
+        printWriter.append(" use append charsequence"); //add to last line
         printWriter.close();
+
+        try {
+            FileWriter fileWriter = new FileWriter("writeFile.txt", true);
+            //drugi parametr informuje czu mamy zastąpić istniejące dane czy dopisywać nowe dane do pliku
+            fileWriter.append("file writer first line");
+            fileWriter.append("file writer second line");
+            fileWriter.append("\n file writer third line");
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Błąd zapisu do pliku");
+        }
     }
 }
